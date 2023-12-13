590157bd18f3612dd5c11b71d61c83d0ceaa1827
Mazaxaka_sf <arty_h41@mail.ru>
2023-12-14 02:06:57 +0300

    setting templates

--- djangositeru/blog/urls.py
+++ djangositeru/blog/urls.py
@@ -4,14 +4,16 @@ from . import converters
 """
 https://proproprogs.ru/django4/django4-dinamicheskie-url-polzovatelskie-konvertery
 все маршруты приложения blog
+name='home' имя маршрута
 """
 register_converter(converters.FourDigitYearConverter, "year4") # регистрируем конвертер для archive
 
 urlpatterns = [
-    path('', views.index, name='home'), # подключаем наш урл + импортируем,  name='home' имя маршрута
-    path('cat/<int:cat_id>/', views.categories),
+    path('', views.index, name='home'), # подключаем наш урл + импортируем,
+    path('about/', views.about, name='about'), # подключаем наш урл + импортируем,
+    path('cat/<int:cat_id>/', views.categories, name='cat_id'),
     # path('cat/', views.categories),  без пагинации
-    path('cat/<slug:cat_slug>/', views.categories_by_slug),
-    path("archive/<year4:year>/", views.archive),
+    path('cat/<slug:cat_slug>/', views.categories_by_slug, name='cat'),
+    path("archive/<year4:year>/", views.archive, name='archive'),
     # с помощью регулярных выражений
 ]
--- djangositeru/blog/views.py
+++ djangositeru/blog/views.py
@@ -1,9 +1,33 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.template.loader import render_to_string
 
 
-def index(request): #функция предстовления
-    return HttpResponse('Hello World')
+menu = ["О сайте","Добавить статью","Обратная связь","Войти"]
+# в шаблоне обращатся через точку
+def index(request):
+    """
+    функция предстовления
+    подключаем шаблон
+    data = title страницы
+    """
+    data = {
+        'title': 'Главная страница',
+        'sub_title': 'Главная страница 2',
+        'menu': menu,
+    }
+    return render(request, 'blog/index.html', context=data)
+
+
+def about(request):
+    """
+    подключаем шаблон 'О себе'
+    data = title страницы
+    """
+    data = {'title': 'О сайте'}
+    return render(request, 'blog/about.html', data)
+
 def categories(request, cat_id):
     """
     :param request: обязательный параметр
@@ -25,6 +49,8 @@ def archive(request, year):
     if year > 2023:
         # перенаправление на главную страницу
         # home имя маршрута
+        # uri = reverse('cat', args=('music',))
+
         return redirect('home')
     return HttpResponse(f'<h1>Архив по годам</h1><p>year: {year}</p>')
 
