2d70382980434927c3527b3abf94226bada9c7a8
Mazaxaka_sf <arty_h41@mail.ru>
2024-01-03 16:25:09 +0300

    requirements3

--- djangositeru/blog/forms.py
+++ djangositeru/blog/forms.py
@@ -5,7 +5,7 @@ from .models import Category, TagPost
 class AddPostForm(forms.Form):
     """
     Добавление статьи
-    .///789
+    .///789 16.224
     required=False // не обязательное поле
     empty_label // штатное название в выборе категории
     initial=True // чекбокс тру
